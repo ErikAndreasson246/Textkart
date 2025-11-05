f7c7008e269a9fdafb839d5547dc3e12d1ca812f
ErikAndreasson246 <14EA0529@amal.se>
2025-11-05 09:08:30 +0100

    Add project files.

---
+++ Textkart/Car.cs
@@ -0,0 +1,15 @@
+﻿class Car
+{
+    public string CarName {  get; set; }
+    public int MaxSpeed { get; set; }
+    public int Turning { get; set; }
+
+
+    public Car(string carName, int maxSpeed, int turning)
+    {
+        CarName = carName;
+        MaxSpeed = maxSpeed;
+        Turning = turning;
+    }
+
+}
---
+++ Textkart/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Runtime.CompilerServices;
+
+class program
+{ 
+    public static void Main(string[] args)
+    {
+        List<Car> cars = new List<Car>();
+
+        bool creatingCar = true;
+        bool isTrue = true;
+
+
+        while (creatingCar)
+        {
+
+
+            int statPoints = 100;
+
+
+
+            Console.WriteLine("Enter the name of the car");
+            string carName = Console.ReadLine();
+
+
+            int speedPoints = GetValue("How many points do you want to spend on speed? 1 point = 5km/h");
+            int maxSpeed = speedPoints * 5;
+            statPoints -= speedPoints;
+
+            int turningPoints = GetValue("How many points do you want to spend on turning?");
+            int turning = turningPoints;
+            statPoints -= speedPoints;
+
+            Car a = new Car(carName, maxSpeed, turning);
+            Console.Clear();
+
+            Console.WriteLine("Name:" + a.CarName + ", Max Speed:" + a.MaxSpeed + "km/h, " + "Turning:" + a.Turning);
+
+            cars.Add(a);
+
+            Console.WriteLine("Do you want to create another car? yes/no");
+
+            while (isTrue)
+            {
+
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "yes")
+                {
+                    isTrue = false;
+                    creatingCar = true;
+                }
+                else if (answer == "no")
+                {
+                    isTrue = false;
+                    creatingCar = false;
+                }
+                else
+                {
+                    Console.WriteLine("You can only answer yes or no");
+                    isTrue = true;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < cars.Count; i ++)
+        {
+            Console.WriteLine("Name:" + cars[i].CarName + ", Max Speed:" + cars[i].MaxSpeed + "km/h, " + "Turning:" + cars[i].Turning);
+        }
+
+        int GetValue(string question)
+        {
+            Console.WriteLine(question);
+            string input = Console.ReadLine();
+            int maxSpeed = Convert.ToInt32(input);
+
+            return maxSpeed;
+        }
+    } 
+}
+        
+        
+ 
